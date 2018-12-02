1bc6af57b9db8ce90df6fae1e4f0138b008270f3
some nerd <31600203+CyberVolcano@users.noreply.github.com>
2018-12-01 18:28:38 -0800

    Update main-script.sh

--- main-script.sh
+++ main-script.sh
@@ -127,10 +127,10 @@ find / -name '*.jpeg' -type f -not -path "/usr/*" >> /CyberPatriot-Linux-Tools/r
 #Makes copies of old files
 cp -f /etc/pam.d/common-password /CyberPatriot-Linux-Tools/old_files
 cp -f /etc/sysctl.conf /CyberPatriot-Linux-Tools/old_files
-cp -f /etc/sysctl.d/10-network-security.conf /CyberPatriot-Linux-Tools/old_files
+cp -f /etc/sysctl.conf /CyberPatriot-Linux-Tools/old_files
 
 cp -f /CyberPatriot-Linux-Tools-master/etc-pam.d/pam.d/common-password /etc/pam.d/common-password
-cp -f / CyberPatriot-Linux-Tools/secure-configurations/etc-sysctl.d/10-network-security.conf
+cp -f / CyberPatriot-Linux-Tools/secure-configurations/etc/sysctl.conf /etc/sysctl.conf
 
 #cp -f /CyberPatriot-Linux-Tools/secure-configurations/etc/sysctl.conf /etc/sysctl.conf
 
