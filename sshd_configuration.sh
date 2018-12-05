e6c0c524ba337b38e893fda430c07efda4885cfc
some nerd <31600203+CyberVolcano@users.noreply.github.com>
2018-12-04 18:49:11 -0800

    replaced mv w/ cp

--- sshd_configuration.sh
+++ sshd_configuration.sh
@@ -1,2 +1,2 @@
 cp -f /etc/ssh/sshd_config /CyberPatriot-Linux-Tools/old_files
-cp -f /CyberPatriot-Linux-Tools/secure-configurations/sshd_config /etc/ssh/sshd_config
+mv -f /CyberPatriot-Linux-Tools/secure-configurations/sshd_config /etc/ssh/sshd_config
