05cc0ec4eedb716e44758569db3c751572a8e44c
leeda <leedarren800@gmail.com>
2020-11-14 13:01:35 -0800

    Update password-policy.sh

--- password-policy.sh
+++ password-policy.sh
@@ -1,11 +1,13 @@
 
 #Account lockout policy
 
+
+
+#setting up pam-cracklib
+#apt-get install libpam-cracklib -y DECAPRECATED FUNCTION
+
+apt-get install libpam-pwquality -y
+
 cp -f /CyberPatriot-Linux-Tools/secure-configurations/etc/login.defs /etc/login.defs
 cp -f /CyberPatriot-Linux-Tools/secure-configurations/etc-pam.d/pam.d/common-password /etc/pam.d/common-password
 cp -f /CyberPatriot-Linux-Tools/secure-configurations/etc-pam.d/pam.d/common-auth /etc/pam.d/common-auth
-sudo faillog -m 10
-
-#setting up pam-cracklib
-apt-get install libpam-cracklib -y
-apt-get install libpam-pwquality -y
