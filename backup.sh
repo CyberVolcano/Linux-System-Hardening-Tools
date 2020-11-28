7417fb5aa488190ddf2e9866fe4b0ef56a2565ae
leeda <leedarren800@gmail.com>
2020-11-28 15:33:32 -0800

    second changes set 2

--- backup.sh
+++ backup.sh
@@ -6,4 +6,4 @@ cp -f /etc/pam.d/common-auth /CyberPatriot-Linux-Tools/old_files/
 cp -f /etc/security/pwquality.conf /CyberPatriot-Linux-Tools/old_files/
 cp -f /etc/login.defs /CyberPatriot-Linux-Tools/old_files/
 cp -f /etc/sysctl.conf /CyberPatriot-Linux-Tools/old_files/
-cp -f /etc/syslog.d/50-default.conf /CyberPatriot-Linux-Tools/old_files/
+cp -f /etc/rsyslog.d/50-default.conf /CyberPatriot-Linux-Tools/old_files/
